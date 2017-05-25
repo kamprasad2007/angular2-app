acbdcbad6b2c1f10b7e4c9eb5b509f932ca7e2ff
manoj <kamprasad2007@gmail.com>
2017-05-26 06:55:59 +0800

    config change

--- server.js
+++ server.js
@@ -44,11 +44,12 @@ const User = sequelize.define('user', {
   }
 });
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
